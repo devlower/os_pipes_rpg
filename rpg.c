ede4f39603114f0c5184666424a316ae8f808abc
devlower <tuanne.assenco@gmail.com>
2022-12-08 14:27:40 -0300

    pipes communication

--- rpg.c
+++ rpg.c
@@ -14,34 +14,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// VERIFICA O SISTEMA OPERACIONAL PARA SUBSTITUIR FUNÇÕES DE LIMPEZA DE TELA, BUFFER E SLEEP
+#if defined(__MINGW32__) || defined(_MSC_VER)
+#define clean_input() fflush(stdin)
+#define clear_screen() system("cls")
+#define sleep() system("timeout 1")
+#else
+#include <stdio_ext.h>
+#define clean_input() __fpurge(stdin)
+#define clear_screen() system("clear")
+#define sleep() system("sleep 1")
+#endif
+
+#define MAXBUFF 1024	  // número de caract. do buffer
+#define MAX_PWD_SIZE 1024 // tamanho máximo de nomenclatura de entrada
+
 // funções de controle de cor para visualização do usuário
-char print_pink(char *s);
-char print_yellow(char *s);
-char print_blue(char *s);
-char print_green(char *s);
-char print_green(char *s);
-char print_red(char *s);
-char print_white(char *s);
-char print_cyan(char *s);
+void print_pink(char *s);
+void print_yellow(char *s);
+void print_blue(char *s);
+void print_green(char *s);
+void print_green(char *s);
+void print_red(char *s);
+void print_white(char *s);
+void print_cyan(char *s);
 
 // variáveis globais para leitura e escrita para os pipes
-int readfd, writefd;
+int readfd, writefd, tmp;
+
 
 int main()
 {
 
-  system("cls");
+  //system("clear");
 
   void instructions();
-  void player_customization();
+  void player_customization_race();
+  void player_customization_class();
+
 
   char option, valid_option;
   int counter = 0,
       descriptor, // usado para criar o processo filho pelo fork
       pipe1[2],  // comunicação, pai -> filho jogador1
       pipe2[2];  // comunicação, filho -> pai  jodagor2
-
-  // defining variables declaration
 
   typedef struct player_attr
   {
@@ -63,7 +79,7 @@ int main()
   char classes_opt[4][10] = {"Warrior", "Mage", "Assassin", "Cleric"};
   char races_opt[3][8] = {"Human", "Elf", "Dwarf"};
 
-  Attack warrior_deck[9];
+Attack warrior_deck[9];
   warrior_deck[0] = (Attack){.attack_name = "Blade of Revenge", .attack_pwr = 10, .description = "Blade of Revenge will cut your opponent with bitter power."};
   warrior_deck[1] = (Attack){.attack_name = "Ancient Sharp Fury", .attack_pwr = 13, .description = "Ancient Sharp Fury will use power of your ancestors to cut your enemy."};
   warrior_deck[2] = (Attack){.attack_name = "Indulgent Sword", .attack_pwr = 9, .description = "Indulgent Sword will graciously hit your enemy."};
@@ -117,6 +133,7 @@ int main()
   cleric_deck[8] = (Attack){.attack_name = "Flying Rocks", .attack_pwr = 8, .description = "Dolor duis adipisicing elit aute pariatur laboris et ex magna reprehenderit tempor fugiat officia."};
   cleric_deck[9] = (Attack){.attack_name = "Wizard's Power", .attack_pwr = 15, .description = "Proident sint in sit velit sit ad ea eiusmod eu exercitation qui."};
 
+
   //   printf("                                    .\n"
   // "                               ::. :. :      \n"
   // "                            .-:..==-==       \n"
@@ -137,15 +154,44 @@ int main()
   // "         .-+*****+**+-                       \n"
   // "             ..::..                          \n");
 
-  instructions();
-  player_customization();
+  getchar();
 
-  // if (pipe(pipe1) < 0 || pipe(pipe2) < 0)
-	// {
-	// 	print_red("\nrpg.c: Erro na chamada do pipe");
-	// 	printf("\nerror: pipe1 = %d pipe2 = %d", pipe(pipe1), pipe(pipe2));
-	// 	exit(0);
-	// }
+  instructions();
+
+
+  if (pipe(pipe1) < 0 || pipe(pipe2) < 0)
+	{
+		print_red("\nrpg.c: Erro na chamada do pipe");
+		printf("\nerror: pipe1 = %d pipe2 = %d", pipe(pipe1), pipe(pipe2));
+		exit(0);
+	}
+
+  if((descriptor = fork()) < 0) {
+
+    print_red("Error: Call FORK()");
+    exit(0);
+
+  } else if(descriptor > 0) { // Processo PAI
+
+    close(pipe1[0]);          // Fecha leitura no pipe1
+    close(pipe2[1]);          // Fecha escrita no pipe2
+
+    player_customization_player_1(pipe2[0], pipe1[1]);
+
+    close(pipe1[1]); // fecha escrita pipe1
+		close(pipe2[0]); // fecha leitura pipe2
+
+  } else {
+
+    close(pipe1[1]); // fecha escrita no pipe1
+		close(pipe2[0]); // fecha leitura no pipe2
+
+		player_customization_player_2(pipe1[0], pipe2[1]); //
+
+		close(pipe1[0]); // fecha leitura no pipe1
+		close(pipe2[1]);
+
+  }
 
   return 0;
 }
@@ -188,14 +234,38 @@ print_blue("|\n"
  print_green("\n\nPress any key to continue...\n");
 
   getchar();
-  fflush(stdin);
+  fflush(stdout);
 }
 
-void player_customization() {
+void player_customization_race() {
+  int opt, valid_option = 1;
+
+  //system("clear");
+  print_blue(" Choose your race by its number:\n\n"
+             " 1) Human\n"
+             " 2) Elf\n"
+             " 3) Dwarf\n\n");
+
+  while(valid_option) {
+    printf(" Your choice: ");
+    scanf("%d", &opt);
+    switch(opt) {
+      case 1: print_blue("\n You choose to be a Human!\n"); valid_option = 0; break;
+      case 2: print_blue("\n You choose to be an Elf!\n"); valid_option = 0; break;
+      case 3: print_blue("\n You choose to be a Dwarf!\n"); valid_option = 0; break;
+      default: print_red("\n\n Please insert a valid oprion.\n\n"); break;
+    }
+  }
+
+    fflush(stdout);
+    getchar();
+}
+
+void player_customization_class() {
   int opt;
   int valid_option = 1;
 
-  system("cls");
+  //system("clear");
   print_blue(" Choose your class by its number: \n\n");
   print_yellow("  1) Warrior\n");
   print_cyan("  2) Mage\n");
@@ -203,58 +273,95 @@ void player_customization() {
   print_green("  4) Cleric\n\n");
 
   while(valid_option) {
-    printf("Your choice: ");
+    printf(" Your choice: ");
     scanf("%d", &opt);
     switch(opt) {
-      case 1: print_yellow("\nYou choose to be a Warrior!"); valid_option = 0; break;
-      case 2: print_cyan("\nYou choose to be a Mage!"); valid_option = 0; break;
-      case 3: print_red("\nYou choose to be an Assassin!"); valid_option = 0; break;
-      case 4: print_green("\nYou choose to be a Cleric!"); valid_option = 0; break;
-      default: print_blue("\nPlease insert a valid option. Press 'Enter' to continue.\n\n"); break;
-   }
-   fflush(stdin);
-   getchar();
+      case 1: print_yellow("\nYou choose to be a Warrior!\n"); valid_option = 0; break;
+      case 2: print_cyan("\nYou choose to be a Mage!\n"); valid_option = 0; break;
+      case 3: print_red("\nYou choose to be an Assassin!\n"); valid_option = 0; break;
+      case 4: print_green("\nYou choose to be a Cleric!\n"); valid_option = 0; break;
+      default: print_red("\nPlease insert a valid option.\n\n"); break;
+    }
   }
+
+  fflush(stdout);
+  getchar();
+ }
+
+ player_customization_player_1(readfd, writefd) {
+
+  int tpm;
+
+    print_green("\n Process 1: Player 1  Customization...\n\n");
+    getchar();
+
+    player_customization_race();
+    getchar();
+    player_customization_class();
+    getchar();
+
+    tmp = 1;
+    write(writefd, tmp, 10);
+    printf(" ... fim do Processo 1\n\n");
+
+ }
+
+ player_customization_player_2(readfd, writefd) {
+
+  int  tmp;
+
+    read(readfd, tmp, 10);
+
+    print_green("\n Process 2: Player 2 Customization...\n\n");
+    getchar();
+
+    player_customization_race();
+    getchar();
+    player_customization_class();
+    getchar();
+
+    printf(" ... fim do Processo 2\n\n");
+
  }
 
 // Funções de controle de cor para visualização do usuário
 // Recebem uma string e formatam sua cor para saída no terminal
-char print_pink(char *s)
+void print_pink(char *s)
 {
   printf("\033[1;35m%s\033[0m", s);
 }
 
-char print_cyan(char *s)
+void print_cyan(char *s)
 {
   printf("\033[1;36m%s\033[0m", s);
 }
 
-char print_yellow(char *s)
+void print_yellow(char *s)
 {
   printf("\033[1;33m%s\033[0m", s);
 }
 
-char print_blue(char *s)
+void print_blue(char *s)
 {
   printf("\033[1;34m%s\033[0m", s);
 }
 
-char print_green(char *s)
+void print_green(char *s)
 {
   printf("\033[1;32m%s\033[0m", s);
 }
 
-char print_red(char *s)
+void print_red(char *s)
 {
   printf("\033[1;31m%s\033[0m", s);
 }
 
-char print_white(char *s)
+void print_white(char *s)
 {
   printf("\033[1;29m%s\033[0m", s);
 }
 
-char print_reset(char *s)
+void print_reset(char *s)
 {
   printf("\033[0m%s\033[0m", s);
 }
